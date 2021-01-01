02e5ca3bceaa03a8818453f39c54b34031986404
Prince Mendiratta <prince.mendi@gmail.com>
2021-01-01 13:07:56 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -89,7 +89,7 @@ ONLINE_CHECK_START_TEXT = get_config(
 # Interval between each check
 REQUEST_INTERVAL = get_config(
     "REQUEST_INTERVAL",
-    "1200"
+    1200
 )
 # path to store LOG files
 LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "DTUAlertBot.log")
