dfefcd37b1516335c7ae8b39252a54244dcb562f
Prince Mendiratta <prince.mendi@gmail.com>
2021-01-01 14:28:41 +0530

    Update broadcast.py

--- bot/plugins/broadcast.py
+++ bot/plugins/broadcast.py
@@ -37,6 +37,7 @@ from bot.mongodb.users import user_list, remove_client_from_db
 from datetime import datetime
 from pyrogram.errors.exceptions import UserIsBlocked, ChatWriteForbidden
 from bot import logging
+from .start_text import last_check
 
 
 def get_mod(client: Client):
@@ -71,12 +72,11 @@ def get_mod(client: Client):
         done="[*] Notice Alert Sent to {}/{} people.\n {} user(s) were removed from database.".format((int(total-failed)),total,failed)
         logging.critical(done)
         sendtelegram(3 ,AUTH_CHANNEL, "https://telegra.ph/file/d88f31ee50c8362e86aa8.mp4", done)
+    last_check = mes2
     looped = threading.Timer(int(REQUEST_INTERVAL),lambda: get_mod(Client))
-    print(looped)
     looped.daemon = True
     looped.start()
-    print(looped)
-    return mes2
+    return last_check
 
 
 def getDocId(notice):
