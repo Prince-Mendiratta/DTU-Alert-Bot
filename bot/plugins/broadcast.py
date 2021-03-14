d483d06d63d65aa536d03fb677d5901fb2ca43d3
Prince Mendiratta <prince.mendi@gmail.com>
2021-03-14 21:43:31 +0530

    Update broadcast.py

--- bot/plugins/broadcast.py
+++ bot/plugins/broadcast.py
@@ -20,21 +20,10 @@ import os
 import os.path
 import requests
 import threading
-from pyrogram import (
-    Client,
-    filters
-)
-from pyrogram.types import (
-    Message
-)
-from bot import (
-    AUTH_CHANNEL,
-    REQUEST_INTERVAL,
-    TG_BOT_TOKEN,
-    MONGO_URL
-)
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from bot import AUTH_CHANNEL, REQUEST_INTERVAL, TG_BOT_TOKEN, MONGO_URL
 from os import path
-from bot.hf.flifi import uszkhvis_chats_ahndler
 from bot.hf.request import request_time
 from bot.mongodb.users import user_list, remove_client_from_db
 from datetime import datetime
@@ -45,7 +34,9 @@ import subprocess
 
 def get_mod(client: Client):
     req_result = request_time(Client)
-    mes2 = "[{}]: DTU Website has not been Updated.\nLast Notice - \n{}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),req_result[1])
+    mes2 = "[{}]: DTU Website has not been Updated.\nLast Notice - \n{}".format(
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"), req_result[1]
+    )
     if req_result[0] == 404:
         logging.info("[*] DTU Website has not been Updated.")
         with open("bot/plugins/check.txt", "w+") as f:
@@ -56,51 +47,61 @@ def get_mod(client: Client):
         broadcast_list = user_list()
         total = len(broadcast_list)
         mongo_url, db1 = MONGO_URL.split("net/")
-        mongo_url = mongo_url + 'net/dtu'
-        os.system("mongoexport --uri={} -c=users --type json --out bot/hf/users_{}".format(mongo_url,datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
+        mongo_url = mongo_url + "net/dtu"
+        os.system(
+            "mongoexport --uri={} -c=users --type json --out bot/hf/users_{}".format(
+                mongo_url, datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+            )
+        )
         time.sleep(10)
-        alerts = 1
         failed = 0
         failed_users = []
-        while alerts < 2:
-            for i in range(0,(total)):
-                try:
-                    pp = "[{}]: DTU Site has been Updated!\n\nLatest Notice Title - \n{}\n\nUnder Tab --> {}\n\nCheers!".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),req_result[3],req_result[5])
-                    send_status = sendtelegram(1, broadcast_list[i], file_id, pp)
-                    if send_status == 200:
-                        i += 1
-                        logging.info("[*] Alert Sent to {}/{} people.".format(i,total))
-                        time.sleep(0.3)
-                    elif send_status == 403:
-                        failed += 1
-                        i += 1
-                        failed_users.append(broadcast_list[i])
-                        time.sleep(0.18)
-                    else:
-                        continue
-                except Exception as e:
-                    logging.error("[*] {}".format(e))
-            alerts += 1
-            time.sleep(1)
-            
-        for us in failed_users:	
+        for i in range(0, (total)):
+            try:
+                pp = "[{}]: DTU Site has been Updated!\n\nLatest Notice Title - \n{}\n\nUnder Tab --> {}\n\nCheers!".format(
+                    datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    req_result[3],
+                    req_result[5],
+                )
+                send_status = sendtelegram(1, broadcast_list[i], file_id, pp)
+                if send_status == 200:
+                    i += 1
+                    logging.info(
+                        "[*] Alert Sent to {}/{} people.".format(i, total))
+                    time.sleep(0.3)
+                elif send_status == 403:
+                    failed += 1
+                    i += 1
+                    failed_users.append(broadcast_list[i])
+                    time.sleep(0.18)
+                else:
+                    continue
+            except Exception as e:
+                logging.error("[*] {}".format(e))
+
+        for us in failed_users:
             remove_client_from_db(us)
-        
+
         os.remove("bot/hf/recorded_status.json")
         time.sleep(2)
-        done="[*] Notice Alert Sent to {}/{} people.\n {} user(s) were removed from database.".format((int(total-failed)),total,failed)
+        done = "[*] Notice Alert Sent to {}/{} people.\n {} user(s) were removed from database.".format(
+            (int(total - failed)), total, failed
+        )
         logging.critical(done)
-        sendtelegram(3 ,AUTH_CHANNEL, "https://telegra.ph/file/d88f31ee50c8362e86aa8.mp4", done)
+        sendtelegram(
+            3, AUTH_CHANNEL, "https://telegra.ph/file/d88f31ee50c8362e86aa8.mp4", done
+        )
     with open("bot/plugins/check.txt", "w+") as f:
         f.write(mes2)
         f.close()
     try:
-        looped = threading.Timer(int(REQUEST_INTERVAL),lambda: get_mod(Client))
+        looped = threading.Timer(
+            int(REQUEST_INTERVAL), lambda: get_mod(Client))
         looped.daemon = True
         looped.start()
     except Exception as e:
         logging.critical(e)
-        sendtelegram(2 ,AUTH_CHANNEL, "_", e)
+        sendtelegram(2, AUTH_CHANNEL, "_", e)
     return mes2
 
 
@@ -112,16 +113,18 @@ def getDocId(notice):
             params={
                 "chat_id": AUTH_CHANNEL,
                 "document": notice,
-                "caption" : "[Logs] New Notice."
-            })
+                "caption": "[Logs] New Notice.",
+            },
+        )
         if r.status_code == 200 and r.json()["ok"]:
-            doc_file_id = r.json()['result']['document']['file_id']
+            doc_file_id = r.json()["result"]["document"]["file_id"]
             return doc_file_id
         else:
             raise Exception
     except Exception as e:
         logging.error(e)
-        logging.info("[*] [{}]: Error Sending Logs File!!.".format(datetime.now()))
+        logging.info(
+            "[*] [{}]: Error Sending Logs File!!.".format(datetime.now()))
         doc_file_id = 0
         sys.exit()
         return doc_file_id
@@ -130,29 +133,22 @@ def getDocId(notice):
 def sendtelegram(tipe, user_id, notice, caption):
     if tipe == 1:
         handler = "Document"
-        pramas={
-            "chat_id": user_id,
-            "document": notice,
-            "caption" : caption
-        }
+        pramas = {"chat_id": user_id, "document": notice, "caption": caption}
     elif tipe == 2:
         handler = "Message"
-        pramas={
+        pramas = {
             "chat_id": user_id,
             "text": caption,
         }
     elif tipe == 3:
         handler = "Animation"
-        pramas={
-            "chat_id": user_id,
-            "animation": notice,
-            "caption" : caption
-        }
+        pramas = {"chat_id": user_id, "animation": notice, "caption": caption}
     try:
         token = TG_BOT_TOKEN
         r = requests.get(
-            "https://api.telegram.org/bot{}/send{}".format(token,handler),
-            params=pramas)
+            "https://api.telegram.org/bot{}/send{}".format(token, handler),
+            params=pramas,
+        )
         logging.info(r.status_code)
         if r.status_code == 200 and r.json()["ok"]:
             return 200
@@ -164,12 +160,24 @@ def sendtelegram(tipe, user_id, notice, caption):
         logging.error(e)
         logging.info("[*] Could not send telegram message.")
         return 69
-    
+
 
 def check_status(user_id, usname):
-    sendtelegram(2, user_id, '_', "Sending an alert in 5 seconds!\nPlease minimize the app if you want to check notification settings.")
+    sendtelegram(
+        2,
+        user_id,
+        "_",
+        "Sending an alert in 5 seconds!\nPlease minimize the app if you want to check notification settings.",
+    )
     time.sleep(6)
     req_result = request_time(Client)
-    sendtelegram(2, user_id, '_', '[*] Last Check - [{}]\nLast Notice - \n{}\nHave a Look - {}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), req_result[1], req_result[2]))
+    sendtelegram(
+        2,
+        user_id,
+        "_",
+        "[*] Last Check - [{}]\nLast Notice - \n{}\nHave a Look - {}".format(
+            datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S"), req_result[1], req_result[2]
+        ),
+    )
     logging.info("[*] {} requested for a status update!".format(usname))
-
